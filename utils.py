379065d1b392d214fd69740835b88a329b5c20e3
rozqlinq <alkeyeva.rozalina@gmail.com>
2024-10-13 14:47:59 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,19 +22,19 @@ def apartment_type(result):
         print('error, c = 0')
     
     elif c == 1:
-        apartment_type = 'однушка'
+        apartment_type = '1-комнатная квартира'
     
     elif c == 2:
-        apartment_type = 'двушка'
+        apartment_type = '2-комнатная квартира'
 
     elif c == 3:
-        apartment_type = 'трёшка'
+        apartment_type = '3-комнатная квартира'
 
     elif c == 4:
-        apartment_type = '4-х комнатная'
+        apartment_type = '4-х комнатная квартира'
 
     elif c == 5:
-        apartment_type = '5-и комнатная'
+        apartment_type = '5-и комнатная квартира'
     
     else: 
         apartment_type = 'дворец'
